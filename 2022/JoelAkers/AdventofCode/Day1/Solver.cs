2456a0fd5746524a5dd5cce75ba85419cd8bf11e
Joel Akers <joel.akers@radiancetech.com>
2022-12-02 20:39:53 -0500

    Joel Akers: Day1 and Day 2 Solutions

---
+++ 2022/JoelAkers/AdventofCode/Day1/Solver.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace AdventofCode.Day1;
+
+internal class Solver
+{
+  public int Solve(string filePath)
+  {
+    var file = File.ReadAllLines(filePath);
+    var maxCalories = 0;
+    var caloriesPerElf = new List<int>();
+    var i = 0;
+    while (i < file.Length)
+    {
+      var currentElfCalories = 0;
+      while (i < file.Length && file[i].Length != 0)
+      {
+        currentElfCalories += int.Parse(file[i]);
+        i++;
+      }
+
+      caloriesPerElf.Add(currentElfCalories);
+
+      i++;
+    }
+    caloriesPerElf.Sort();
+    return caloriesPerElf.TakeLast(3).Sum();
+  }
+}
