ba2719a5eabdf641aa23518e15a39f30c2ee06ed
14JAW <joelwakers@cedarville.edu>
2022-12-03 10:27:06 -0500

    JOELAKERS: Day3

--- 2022/JoelAkers/AdventofCode/Day1/Solver.cs
+++ 2022/JoelAkers/AdventofCode/Day1/Solver.cs
@@ -23,6 +23,17 @@ internal class Solver
 
       i++;
     }
+
+    return Part2(caloriesPerElf);
+  }
+
+  private static int Part1(IEnumerable<int> caloriesPerElf)
+  {
+    return caloriesPerElf.Max();
+  }
+
+  private static int Part2(List<int> caloriesPerElf)
+  {
     caloriesPerElf.Sort();
     return caloriesPerElf.TakeLast(3).Sum();
   }
--- 2022/JoelAkers/AdventofCode/Program.cs
+++ 2022/JoelAkers/AdventofCode/Program.cs
@@ -1,10 +1,10 @@
-﻿using AdventofCode.Day2;
+﻿using AdventofCode.Day3;
 
-Console.WriteLine("Day 2");
-//var file = "C:\\Users\\akers\\OneDrive\\Documents\\2022\\AdventOfCode\\AdventOfCode\\2022\\JoelAkers\\AdventofCode\\Day2\\PracticeData.txt";
-var file = "C:\\Users\\akers\\OneDrive\\Documents\\2022\\AdventOfCode\\AdventOfCode\\2022\\JoelAkers\\AdventofCode\\Day2\\RealData.txt";
+Console.WriteLine("Day 3");
+//var file = "C:\\Users\\akers\\OneDrive\\Documents\\2022\\AdventOfCode\\AdventOfCode\\2022\\JoelAkers\\AdventofCode\\Day3\\PracticeData.txt";
+var file = "C:\\Users\\akers\\OneDrive\\Documents\\2022\\AdventOfCode\\AdventOfCode\\2022\\JoelAkers\\AdventofCode\\Day3\\RealData.txt";
 
 var solver = new Solver();
-var result = solver.Solve(file);
+var result = solver.Solve(file, 2);
 
 Console.WriteLine($"Result is: {result}");
