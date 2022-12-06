12cec43273036ab1cc0a35c2bb8e69d9de5468d2
14JAW <joelwakers@cedarville.edu>
2022-12-06 08:32:50 -0500

    JOELAKERS: Day6

---
+++ 2022/JoelAkers/AdventofCode/Day6/Solver.cs
@@ -0,0 +1,70 @@
+﻿using MoreLinq;
+namespace AdventofCode.Day6;
+
+public class Solver
+{
+  public void Solve(string filePath, int partNumber)
+  {
+    var fileLines = File.ReadAllLines(filePath);
+    var result = partNumber switch
+    {
+      2 => DoPart2(fileLines),
+      1 => DoPart1(fileLines)
+    };
+    Console.Write($"Result is: {result}");
+  }
+
+  private int DoPart2(string[] lines)
+  {
+    var firstLine = lines[0];
+    var i = 0;
+    var subPart = new Queue<char>();
+    while (i < firstLine.Length - 13)
+    {
+      var nextLetter = firstLine[i];
+      if (subPart.Contains(nextLetter))
+      {
+        while (subPart.Dequeue() != nextLetter) { };
+        subPart.Enqueue(nextLetter);
+      }
+      else if (subPart.Count < 13)
+      {
+        subPart.Enqueue(nextLetter);
+      }
+      else
+      {
+        return i + 1;
+      }
+
+      i++;
+    }
+    return 0;
+  }
+
+  private int DoPart1(string[] lines)
+  {
+    var firstLine = lines[0];
+    var i = 0;
+    var subPart = new Queue<char>();
+    while (i < firstLine.Length - 3)
+    {
+      var nextLetter = firstLine[i];
+      if (subPart.Contains(nextLetter))
+      {
+        while(subPart.Dequeue() != nextLetter) {};
+        subPart.Enqueue(nextLetter);
+      }
+      else if (subPart.Count < 3)
+      {
+        subPart.Enqueue(nextLetter);
+      }
+      else
+      {
+        return i + 1;
+      }
+
+      i++;
+    }
+    return 0;
+  }
+}
--- 2022/JoelAkers/AdventofCode/Program.cs
+++ 2022/JoelAkers/AdventofCode/Program.cs
@@ -1,8 +1,8 @@
-﻿using AdventofCode.Day5;
+﻿using AdventofCode.Day6;
 
-Console.WriteLine("Day 5");
-//var file = "C:\\Users\\akers\\OneDrive\\Documents\\2022\\AdventOfCode\\AdventOfCode\\2022\\JoelAkers\\AdventofCode\\Day5\\PracticeData.txt";
-var file = "C:\\Users\\akers\\OneDrive\\Documents\\2022\\AdventOfCode\\AdventOfCode\\2022\\JoelAkers\\AdventofCode\\Day5\\RealData.txt";
+Console.WriteLine("Day 6");
+//var file = "C:\\Users\\akers\\OneDrive\\Documents\\2022\\AdventOfCode\\AdventOfCode\\2022\\JoelAkers\\AdventofCode\\Day6\\PracticeData.txt";
+var file = "C:\\Users\\akers\\OneDrive\\Documents\\2022\\AdventOfCode\\AdventOfCode\\2022\\JoelAkers\\AdventofCode\\Day6\\RealData.txt";
 
 var solver = new Solver();
 solver.Solve(file, 2);
